97d75263b6a4314e248a6fb47ed4f4c45b3bab61
Oribuin <>
2020-11-06 07:32:46 +0000

    Webhook system

--- src/main/kotlin/xyz/oribuin/lilori/command/author/CmdTest.kt
+++ src/main/kotlin/xyz/oribuin/lilori/command/author/CmdTest.kt
@@ -1,10 +1,17 @@
 package xyz.oribuin.lilori.command.author
 
+import com.google.gson.JsonObject
+import net.dv8tion.jda.api.entities.Icon
 import xyz.oribuin.lilori.LilOri
-import xyz.oribuin.lilori.handler.Category
-import xyz.oribuin.lilori.handler.CommandInfo
 import xyz.oribuin.lilori.handler.BotCommand
+import xyz.oribuin.lilori.handler.Category
 import xyz.oribuin.lilori.handler.CommandEvent
+import xyz.oribuin.lilori.handler.CommandInfo
+import xyz.oribuin.lilori.util.BotUtils
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.TimeUnit
 
 @CommandInfo(
         name = "Test",
@@ -15,12 +22,39 @@ import xyz.oribuin.lilori.handler.CommandEvent
         userPermissions = [],
         botPermissions = [],
         guildId = "",
-        ownerOnly = true
+        ownerOnly = false
 )
 class CmdTest(bot: LilOri) : BotCommand(bot) {
 
     override fun executeCommand(event: CommandEvent) {
-        event.reply("Test")
 
+        if (event.args.size < 2) {
+            event.sendEmbedReply("❗ Invalid Arguments", "The correct usage is ${event.prefix}${getAnnotation(javaClass).name.toLowerCase()} ${BotUtils.formatList(getAnnotation(javaClass).arguments.toList())}")
+            return
+        }
+
+        val msg = java.lang.String.join(" ", *event.args).substring(event.args[0].length + 1)
+
+        event.textChannel.createWebhook(event.author.name)
+                .setAvatar(Icon.from(URL(event.author.avatarUrl ?: event.author.defaultAvatarUrl).openStream()))
+                .queue {
+                    val json = JsonObject()
+                    json.addProperty("content", msg)
+
+                    val connection = URL(it.url).openConnection() as HttpURLConnection
+                    connection.requestMethod = "POST"
+                    connection.setRequestProperty("Content-Type", "application/json")
+                    connection.doOutput = true
+                    connection.outputStream.use { out ->
+                        out.write(json.toString().toByteArray(StandardCharsets.UTF_8))
+                        out.flush()
+                    }
+
+                    connection.inputStream.close()
+                    connection.disconnect()
+                    event.deleteCmd()
+
+                    it.delete().queueAfter(3, TimeUnit.SECONDS)
+                }
     }
 }
