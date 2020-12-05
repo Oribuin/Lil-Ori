f6b1ae3d099f4fcaed7e3b7fbcf649a1c7073e8a
Oribuin <>
2020-12-05 10:23:02 +0000

    Make CommandInfo annotation readable

--- src/main/kotlin/xyz/oribuin/lilori/handler/CommandInfo.kt
+++ src/main/kotlin/xyz/oribuin/lilori/handler/CommandInfo.kt
@@ -2,4 +2,14 @@ package xyz.oribuin.lilori.handler
 
 import net.dv8tion.jda.api.Permission
 
-annotation class CommandInfo(val name: String, val description: String, val category: Category.Type, val arguments: Array<String>, val aliases: Array<String>, val userPermissions: Array<Permission>, val botPermissions: Array<Permission>, val guildId: String, val ownerOnly: Boolean = false)
+annotation class CommandInfo(
+        val name: String,
+        val description: String,
+        val category: Category.Type,
+        val arguments: Array<String>,
+        val aliases: Array<String>,
+        val userPermissions: Array<Permission>,
+        val botPermissions: Array<Permission>,
+        val guildId: String,
+        val ownerOnly: Boolean = false
+)
